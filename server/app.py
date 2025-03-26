c9a6d16fe3eeddd846be562ce1c8ba2340cc5309
Eric Chang <changyungta2001@gmail.com>
2025-03-26 14:31:53 -0500

    add backend code for project creation and information retrieval

--- server/app.py
+++ server/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 import datetime
 from flask_cors import CORS
-# import db_utils
 import sys
 sys.path.append('../')
-import database.db_access as db_utils
+import database.db_access as db_access
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {
@@ -27,9 +26,10 @@ api = Api(
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
-# Define a namespace for authentication-related routes
+# Namespace for different routes
 auth_ns = api.namespace("auth", description="User Authentication Endpoints")
 resources_ns = api.namespace("resources", description="Resource Management Endpoints")
+project_ns = api.namespace("projects", description="Project Access Endpoints")
 
 # Define User Registration and Login Models for Swagger
 user_model = api.model(
@@ -56,7 +56,7 @@ login_model = api.model(
 checkout_model = api.model(
     "Checkout",
     {
-        "token": fields.String(required=True, description="JWT Access Token"),
+        "projectID": fields.String(required=True, description="Identify project to checkout resource"),
         "hardware_set": fields.String(required=True, description="Targeted hardware set"),
         "amount": fields.Integer(required=True, description="Amount to check out"),
     }
@@ -68,6 +68,23 @@ checkin_model = api.model(
         "token": fields.String(required=True, description="JWT Access Token"),
         "hardware_set": fields.String(required=True, description="Targeted hardware set"),
         "amount": fields.Integer(required=True, description="Amount to check in"),
+    }
+)
+
+project_create_model = api.model(
+    "Project creation",
+    {
+        "name": fields.String(required=True, description="Name of the group"),
+        "des": fields.String(required=True, description="Description of the group"),
+        "projectID": fields.Integer(required=True, description="Unique ID of the group"),
+    }
+)
+
+get_project_model = api.model(
+    "Project creation",
+    {
+        "token": fields.String(required=True, description="JWT Access Token"),
+        "projectID": fields.Integer(required=True, description="Unique ID of the group"),
     }
 )
 
@@ -98,19 +115,21 @@ def home():
 class Register(Resource):
     @api.expect(user_model)
     @api.response(201, "User registered successfully")
-    @api.response(400, "User already exists", error_response)
+    @api.response(400, "User or email already exists", error_response)
     def post(self):
         """Register a new user"""
         data = request.json
         username, email, password = data["username"], data["email"], data["password"]
 
-        if db_utils.checkEmailExist(email):
-            return {"message": "User already exists"}, 400
+        if db_access.checkEmailExist(email):
+            return {"message": "User or email already exists"}, 400
 
-        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-        db_utils.addUser(username, email, hashed_password)
-
-        return {"message": "User registered successfully"}, 201
+        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")  
+        res = db_access.addUser(username, email, hashed_password)
+        if res == 0:
+            return {"message": "User registered successfully"}, 201
+        else:
+            return {"message": "User or email already exists"}, 400
 
 
 @auth_ns.route("/login", methods=['POST'])
@@ -123,8 +142,8 @@ class Login(Resource):
         data = request.json
         email, password = data["email"], data["password"]
 
-        if not db_utils.checkEmailExist(email) or not bcrypt.check_password_hash(
-            db_utils.getUserHashedPassword(email), password
+        if not db_access.checkEmailExist(email) or not bcrypt.check_password_hash(
+            db_access.getUserHashedPassword(email), password
         ):
             return {"message": "Invalid credentials"}, 401
 
@@ -136,7 +155,7 @@ class Login(Resource):
 @resources_ns.route("/", methods=['GET'])
 class Resources(Resource):
     def get(self):
-        return db_utils.getAvailHardwares()
+        return db_access.getAvailHardwares(), 200
 
 @resources_ns.route("/checkout", methods=['POST'])
 class CheckOut(Resource):
@@ -148,14 +167,14 @@ class CheckOut(Resource):
     def post(self):
         """Check out hardware resources"""
         data = request.json
-        hardware_set, amount = data["hardware_set"], data["amount"]
+        projectID, hardware_set, amount = data["projectID"], data["hardware_set"], data["amount"]
         # check for jwt token validity
         email = get_jwt_identity()
         # check if hardware available
-        if not db_utils.checkHardwareAvail(hardware_set, amount):
+        if not db_access.checkHardwareAvail(hardware_set, amount):
             return {"message": "Insufficient hardware resources"}, 503
         
-        if db_utils.checkoutHardwares(email, hardware_set, amount):
+        if db_access.checkoutHardwares(projectID, hardware_set, amount):
             return {"message": f"Successfully check out {amount} from {hardware_set}"}, 202
         else:
             return {"message": "Check out failed"}, 503
@@ -171,20 +190,56 @@ class CheckIn(Resource):
     def post(self):
         """Check in hardware resources"""
         data = request.json
-        hardware_set, amount = data["hardware_set"], data["amount"]
+        projectID, hardware_set, amount = data["projectID"], data["hardware_set"], data["amount"]
         # check for jwt token validity
         email = get_jwt_identity()
 
         # check if hardware available
-        if not db_utils.checkAssignedHardwares(email, hardware_set, amount):
+        if not db_access.checkAssignedHardwares(projectID, hardware_set, amount):
             return {"message": "No enough hardware to check in"}, 503
 
-        if db_utils.checkInHardwares(email, hardware_set, amount):
+        if db_access.checkInHardwares(projectID, hardware_set, amount):
             return {"message": f"Successfully check in {amount} of {hardware_set}"}, 202
         else:
             return {"message": "Check in failed"}, 503
-            
 
+@project_ns.route("/create", methods=['POST'])            
+class CreateProject(Resource):
+    @api.expect(project_create_model)
+    @api.response(200, "Project successfully created")
+    @api.response(400, "Non-unique project ID")
+    @api.response(400, "Unable to complete the request")
+    def post(self):
+        """Create a new project"""
+        data = request.json
+        name, des, projectID = data["name"], data["des"], data["projectID"]
+
+        res = db_access.createProject(name, des, projectID)
+        if res == -1:
+            return {"message": "Non-unique project ID"}, 400
+        elif res == 0:
+            return {"message": "Project successfully created"}, 200
+        else:
+            return {"message": "Unable to complete the request"}, 400
+
+@project_ns.route("/get_project", methods=['GET'])  
+class GetProject(Resource):
+    @jwt_required()
+    @api.expect(get_project_model)
+    @api.response(200, "Project information found and returned")
+    @api.response(400, "Project not found or invalid credentials")
+    def get(self):
+        """Get the information of a project"""
+        data = request.json
+        projectID = data["projectID"]
+        email = get_jwt_identity()
+
+        res = db_access.getProject(email, projectID)
+        if res == None:
+            return {"message": "Invalid operations"}, 400
+        else:
+            return res['users'], 200
+        
 
 # Add namespaces to API
 api.add_namespace(auth_ns, path="/auth")
@@ -196,5 +251,4 @@ if __name__ == "__main__":
 """
 
 if __name__ == "__main__":
-    # db_utils.db_init()
     app.run(debug=True)
